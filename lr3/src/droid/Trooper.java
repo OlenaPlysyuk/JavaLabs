1e8458328610c275fff479ab6f88f59e0436b027
OlenaPlysyuk <136686620+OlenaPlysyuk@users.noreply.github.com>
2023-10-20 00:35:19 +0300

    Delete lr3 directory

--- lr3/src/droid/Trooper.java
+++
@@ -1,7 +0,0 @@
-package droid;
-
-public class Trooper extends Droid{
-    public Trooper(int health, int damage, int cr_chance,String name) {
-        super(health, damage, cr_chance,name);
-    }
-}
